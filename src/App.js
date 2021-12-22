02c92440d1c535c649087f04ac130d68526373c8
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2021-12-22 17:35:36 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+
+import "./App.css";
+import ExamplePage from "./pages/examplePage/ExamplePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <ApolloProvider client={client}>
+    <ApolloProvider>
+      {/* <Provider store={store}> */}
+      <Provider>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
+        <PersistGate loading={null}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" exact element={<ExamplePage />} />
+            </Routes>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ApolloProvider>
   );
 }
 
