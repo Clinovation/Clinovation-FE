e6ad37d0c373dcd6897e76edf4da30d8c99eaebe
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2022-01-18 21:08:03 +0700

    add functionality to get all doctors

--- src/components/CardListPatientDoctorComponents/CardListPatientDoctor.jsx
+++ src/components/CardListPatientDoctorComponents/CardListPatientDoctor.jsx
@@ -1,67 +1,266 @@
-import React from 'react'
-import { Button,Card, Container, Row, InputGroup, FormControl, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import Avatar from '../../icons/staffProfile.png'
-import SideBarMedStaff from '../SideBarMedStaffComponents/SideBarMedStaff'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  GenerateAxiosConfig,
+  HandleDate,
+  HandleLowerCase,
+  HandleUnauthorized,
+} from "../../utils/helpers";
+import {
+  Button,
+  Card,
+  Container,
+  Row,
+  InputGroup,
+  FormControl,
+  Pagination,
+  Col,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../icons/staffProfile.png";
+import SideBarMedStaff from "../SideBarMedStaffComponents/SideBarMedStaff";
 
 function CardListPatientDoctor() {
-    return (
-        <div>
-            <Container fluid>
-                <Row>
-                    <Col md="1">
-                        <SideBarMedStaff/>
-                    </Col>
+  const checkName = / ^(([A-Za-z]+[,.]?[ ]?|[a-z]+['-]?)+)$ /;
+  const [patient, setPatient] = useState({
+    by: "",
+    data: [],
+    currPage: 1,
+    pages: [],
+  });
+  const [error, setError] = useState();
 
-                    <Col md="11">
-                        <Container className='mt-3' style={{width: '900px'}}> 
-                            <Row >
-                                <div class="d-flex bd-highlight" >
-                                    <div class="p-2 bd-highlight"><h3>Patient Lists</h3></div>
-                                
-                                    <div class="ms-auto p-2 bd-highlight">
-                                        <InputGroup className="mb-3" size="sm" style={{width: '300px'}}>
-                                            <FormControl
-                                            aria-label="Recipient's username"
-                                            aria-describedby="basic-addon2"
-                                            />
-                                            <Button variant="outline-secondary" id="button-addon2">
-                                            Search
-                                            </Button>
-                                        </InputGroup>
-                                    </div>
+  const fetch = (page, by) => {
+    const API_URL = "http://3.83.92.188:8080/api/v1";
+    if (patient.by === "") {
+      axios
+        .get(`${API_URL}/doctor/?page=${page}`, GenerateAxiosConfig())
+        .then((res) => {
+          if (res.status === 204) {
+            setError("No record found");
+          } else {
+            const page = { ...res.data.page };
+            const length = page.total_data / page.limit;
+            const active = page.offset / page.limit + 1;
+            const items = [];
+            for (let i = 0; i < length; i++) {
+              items.push(i + 1);
+            }
+            setPatient((state) => {
+              return {
+                ...state,
+                data: res.data.data,
+                currPage: active,
+                pages: items,
+              };
+            });
+          }
+        })
+        .catch((error) => {
+          if (error.response) {
+            HandleUnauthorized(error.response);
+            setError(error.response.data.meta.messages[0]);
+            console.log(error);
+          }
+        });
+    } else if (checkName.test(patient.by)) {
+      axios
+        .get(
+          `${API_URL}/doctor/?name=${by}&page=${page}`,
+          GenerateAxiosConfig()
+        )
+        .then((res) => {
+          if (res.status === 204) {
+            setError("No record found");
+          } else {
+            const page = { ...res.data.page };
+            const length = page.total_data / page.limit;
+            const active = page.offset / page.limit + 1;
+            const items = [];
+            for (let i = 0; i < length; i++) {
+              items.push(i + 1);
+            }
+            setPatient((state) => {
+              return {
+                ...state,
+                data: res.data.data,
+                currPage: active,
+                pages: items,
+              };
+            });
+          }
+        })
+        .catch((error) => {
+          if (error.response) {
+            HandleUnauthorized(error.response);
+            setError(error.response.data.meta.messages[0]);
+            console.log(error);
+          }
+        });
+    } else {
+      axios
+        .get(`${API_URL}/doctor/?nik=${by}&page=${page}`, GenerateAxiosConfig())
+        .then((res) => {
+          if (res.status === 204) {
+            setError("No record found");
+          } else {
+            const page = { ...res.data.page };
+            const length = page.total_data / page.limit;
+            const active = page.offset / page.limit + 1;
+            const items = [];
+            for (let i = 0; i < length; i++) {
+              items.push(i + 1);
+            }
+            setPatient((state) => {
+              return {
+                ...state,
+                data: res.data.data,
+                currPage: active,
+                pages: items,
+              };
+            });
+          }
+        })
+        .catch((error) => {
+          if (error.response) {
+            HandleUnauthorized(error.response);
+            setError(error.response.data.meta.messages[0]);
+            console.log(error);
+          }
+        });
+    }
+  };
+
+  useEffect(() => {
+    fetch(1, "");
+  }, [setPatient]);
+
+  const handlePage = (index) => {
+    fetch(index, patient.by);
+  };
+  const onChange = (e) => {
+    const value = e.target.value;
+    setPatient({ ...patient, by: value });
+  };
+  console.log(patient);
+  return (
+    <div>
+      <Container fluid>
+        <Row>
+          <Col md="1">
+            <SideBarMedStaff />
+          </Col>
+
+          <Col md="11">
+            <Container className="mt-3" style={{ width: "900px" }}>
+              <Row>
+                <div class="d-flex bd-highlight">
+                  <div class="p-2 bd-highlight">
+                    <h3>Patient Lists</h3>
+                  </div>
+
+                  <div class="ms-auto p-2 bd-highlight">
+                    <InputGroup
+                      className="mb-3"
+                      size="sm"
+                      style={{ width: "300px" }}
+                    >
+                      <FormControl
+                        aria-label="search"
+                        aria-describedby="basic-addon2"
+                        type="search"
+                        name="search"
+                        value={patient.by}
+                        onChange={onChange}
+                      />
+                      <Button
+                        variant="outline-secondary"
+                        id="button-addon2"
+                        onClick={handlePage}
+                      >
+                        Search
+                      </Button>
+                    </InputGroup>
+                  </div>
+                </div>
+              </Row>
+
+              {error && <p className="text-center text-light mt-5">{error}</p>}
+              {patient?.data?.map((item) => (
+                <Row>
+                  <div className="d-flex justify-content-center mt-5">
+                    <Card
+                      key={item.id}
+                      style={{ marginBottom: "10px", width: "900px" }}
+                    >
+                      <Card.Body style={{ overflowX: "auto" }}>
+                        <div class="d-flex bd-highlight">
+                          <div class="p-2 bd-highlight">
+                            {item.avatar === "" ? (
+                              <img src={Avatar} style={{ height: "56px" }} />
+                            ) : (
+                              <img
+                                src={item.avatar}
+                                style={{ height: "56px" }}
+                              />
+                            )}
+                          </div>
+                          <div className="p-2 bd-highlight mt-3">
+                            <h6 style={{ marginRight: "45px" }}>{item.name}</h6>
+                          </div>
+                          <div className="p-2 bd-highlight mt-3">
+                            <h6 style={{ marginRight: "45px" }}>{item.dob}</h6>
+                          </div>
+                          <div className="p-2 bd-highlight mt-3">
+                            <h6>{item.nik}</h6>
+                          </div>
+                          <div className="ms-auto p-2 bd-highlight mt-3">
+                            <Link to={`/medicalRecord/${item.uuid}`}>
+                              <Button
+                                variant="info"
+                                size="sm"
+                                style={{ marginRight: "30px" }}
+                              >
+                                <div style={{ color: "#ffffff" }}>
+                                  Patient Record
                                 </div>
-                            </Row>
-                                
-                            <Row>
-                                <div className='d-flex justify-content-center mt-5'>
-                                
-                                    <Card style={{ marginBottom: '10px', width : "900px" }}>
-                                        <Card.Body  style={{ overflowX : "auto" }}>
-                                            <div class="d-flex bd-highlight">
-                                                <div class="p-2 bd-highlight"><img src={Avatar} style={{height: "56px"}}/></div>
-                                                <div className="p-2 bd-highlight mt-3"><h6 style={{marginRight: "45px"}}>Ralph</h6></div>
-                                                <div className="p-2 bd-highlight mt-3"><h6 style={{marginRight: "45px"}}>32 Years old</h6></div>
-                                                <div className="p-2 bd-highlight mt-3"><h6>710481048018</h6></div>
-                                                <div className="ms-auto p-2 bd-highlight mt-3">
-                                                    <Link to="/medicalRecord">
-                                                        <Button variant="info" size="sm" style={{marginRight: "30px"}}><div style={{color: "#ffffff"}}>Patient Record</div></Button>
-                                                    </Link>
-                                                    <Link to="/inputConsultation">
-                                                        <Button variant="info" size="sm"><div style={{color: "#ffffff"}}>Add New Consultation</div></Button>
-                                                    </Link>
-                                                </div> 
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
+                              </Button>
+                            </Link>
+                            <Link to={`/inputConsultation/${item.uuid}`}>
+                              <Button variant="info" size="sm">
+                                <div style={{ color: "#ffffff" }}>
+                                  Add New Consultation
                                 </div>
-                            </Row>
-                        </Container>
-                    </Col>
+                              </Button>
+                            </Link>
+                          </div>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </div>
                 </Row>
+              ))}
+              <div className="d-flex justify-content-center">
+                {patient && (
+                  <Pagination className="align-self-center">
+                    {patient.pages.map((item) => (
+                      <Pagination.Item
+                        key={item}
+                        active={item === patient.currPage}
+                        onClick={() => handlePage(item)}
+                      >
+                        {item}
+                      </Pagination.Item>
+                    ))}
+                  </Pagination>
+                )}
+              </div>
             </Container>
-        </div>
-    )
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default CardListPatientDoctor
+export default CardListPatientDoctor;
